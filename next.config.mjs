acc24bc821be05ef210e9ccbb68caffa219bf473
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-08-12 01:19:23 +0900

    Exec ESLint in GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // https://nextjs.org/docs/app/building-your-application/configuring/eslint#linting-custom-directories-and-files
+  eslint: {
+    dirs: ["pages", "app", "components", "lib", "src", "tests"],
+  },
 }
 
 export default nextConfig
