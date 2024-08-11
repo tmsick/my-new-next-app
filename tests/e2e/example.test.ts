22b83930a577da0f1b1097ddb852078a3f805060
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-08-12 02:14:12 +0900

    Init MUI

--- tests/e2e/example.test.ts
+++ tests/e2e/example.test.ts
@@ -6,3 +6,9 @@ test("has title", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Create Next App/)
 })
+
+test("has h1", async ({ page }) => {
+  await page.goto("http://localhost:3000/")
+  const h1 = page.locator("h1").first()
+  await expect(h1).toHaveText("Hello, World")
+})
